d69cf472bd4f468ff95ea354cb0acf9a0c241e77
EC2 Default User <ec2-user@ip-10-0-0-28.ap-northeast-1.compute.internal>
2023-07-19 13:03:58 +0000

    todo_controller

---
+++ app/controllers/api / v1/todos_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::TodosController < ApplicationController
+    def index
+      todos=Todo.order(update_at: :desc)
+      #アップデート順で並べたい
+      render json: todos
+    end
+    def show
+      todo = Todo.find(params[:id])
+      render json: todo
+    end
+    def create
+      todo=Todo.new(todo_parmas)
+      if todo.save
+          render json: todo
+      else 
+           render json: todo.errors,status: 422
+      end
+    end
+    def update
+         todo = Todo.find(params[:id])
+         if todo.update(todo_parmas)
+             render json: todo
+         else
+             render json: todo.errors,status: 422
+         end
+    end
+    def destroy
+        if Todo.destroy(params[:id])
+          head :no_content#返すものがないので、
+        else
+          render json: { error: "Failed to destroy" }, status: 422
+        end
+    
+    end
+    def destroy_all
+        if Todo.destroy_all
+         head :no_content
+        else
+            render json: { error: "Failed to destroy" }, status: 422
+        end
+    end
+private   
+    def todo_params
+        params.require(:todo).permit(:name,:is_completed)
+    end
+end
